ddbb85f515b9cf8f123ef08b7ab3e13f678e67d4
Nishi Sheth <nishivsheth@gmail.com>
2019-06-18 21:53:01 +1000

    adding react redux code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import { startAction } from "actions/startAction";
+import { stopAction } from "actions/stopAction";
+
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img 
+  src={logo} 
+  className={
+    "App-logo" + 
+    (this.props.rotating ? "":" App-logo-paused")
+  } 
+  alt="logo" 
+  onClick={
+    this.props.rotating ? 
+      this.props.stopAction : this.props.startAction
+  }
+/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// new imports start
+import { Provider } from "react-redux";
+
+import configureStore from "store";
+// new imports stop
+
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// changed the render
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+// changed the render
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
